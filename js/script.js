e086e39b999f29d89c6da1cac043da4670375bcf
uddinmayraj <mayrajuddin03@gmail.com>
2022-04-16 03:20:51 +0600

    add

--- js/script.js
+++ js/script.js
@@ -3,4 +3,24 @@ $(function(){
         delay: 30,
         time: 1000
     })
+    // prggress bar  js start
+    $('.proggressBar').rProgressbar({
+        percentage: 80,
+        duration: 2000,
+        fillBackgroundColor: '#15c8a0',
+        backgroundColor: '#eff5f4',
+        borderRadius: '0px',
+        height: '16px',
+        width: '100%'
+    });
+    $('.proggressBarnd').rProgressbar({
+        percentage: 40,
+        duration: 2000,
+        fillBackgroundColor: '#15c8a0',
+        backgroundColor: '#eff5f4',
+        borderRadius: '0px',
+        height: '16px',
+        width: '100%'
+    })
+    // prggress bar  js end
 })
